1ff0731557d3a5a430e0b3656af4fdaedcf08169
Mustafa Tola <mustafatola@Mustafas-MacBook-Pro.local>
2020-07-30 01:50:23 +0300

    P0000 - morgan is added, error handling completed

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const app = express();
 
+// logger helper
+const morgan = require('morgan');
+
+app.use(morgan('dev'));
+
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+});
+
 module.exports = app;
+
