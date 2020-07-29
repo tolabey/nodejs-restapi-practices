4fe814495dfb45c62cea371d896844309c197127
Mustafa Tola <mustafatola@Mustafas-MacBook-Pro.local>
2020-07-30 02:17:04 +0300

    Parsing body with body parser, Cors handling

--- app.js
+++ app.js
@@ -3,12 +3,30 @@ const app = express();
 
 // logger helper
 const morgan = require('morgan');
+//parse request body as json
+const bodyParser = require('body-parser');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+// Cors Handling
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT', 'POST', 'PATCH', 'DELETE', 'GET');
+        return res.status(200).json({})
+    }
+
+    next();
+});
+
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+// routers
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
@@ -18,6 +36,7 @@ app.use((req, res, next) => {
     next(error);
 });
 
+// generic error handling.
 app.use((error, req, res, next) => {
     res.status(error.status || 500);
     res.json({
